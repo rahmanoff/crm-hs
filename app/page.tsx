cbaa55518690677af40803ca552d5c606ccc4bcf
Kostiantyn Rakhmanov <rahmanoff@gmail.com>
2025-07-02 15:47:08 +0300

    main screen 2xl

--- app/page.tsx
+++ app/page.tsx
@@ -272,7 +272,7 @@ export default function Home() {
         </div>
       </header>
 
-      <main className='max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 py-8'>
+      <main className='max-w-screen-2xl mx-auto px-4 sm:px-6 lg:px-8 py-8'>
         <motion.div
           variants={containerVariants}
           initial='hidden'
